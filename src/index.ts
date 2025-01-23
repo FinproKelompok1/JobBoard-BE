5e9ef62783e1cd3efe129fb53af0a67040a94d28
Hanif Ahmad <ahmadhanif759@gmail.com>
2025-01-23 17:33:31 +0700

    first init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const PORT: number = 8000;
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api", (req: Request, res: Response) => {
+  res.status(200).send("Server Connected");
+});
+
+app.listen(PORT, () =>
+  console.log(`Your server is running on http://localhost:${PORT}/api`)
+);
