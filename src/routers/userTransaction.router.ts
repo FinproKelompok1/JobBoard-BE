2f14ccfe61191058b663b18ec73c48fa5081a6e2
muhammadwildansapoetro <muhwilsap@gmail.com>
2025-02-22 22:20:22 +0700

    update

--- src/routers/userTransaction.router.ts
+++ src/routers/userTransaction.router.ts
@@ -1,25 +1,25 @@
-import { Router } from "express";
-import { UserTransactionController } from "../controllers/userTransaction.controller";
-import { requireAuth } from "../middleware/auth.middleware";
+// import { Router } from "express";
+// // import { UserTransactionController } from "../controllers/userTransaction.controller";
+// import { requireAuth } from "../middleware/auth.middleware";
 
-export class UserTransactionRouter {
-  private router: Router;
-  private userTransactionController: UserTransactionController;
+// export class UserTransactionRouter {
+//   private router: Router;
+//   private userTransactionController: UserTransactionController;
 
-  constructor() {
-    this.router = Router();
-    this.userTransactionController = new UserTransactionController();
-    this.initialiazeRoutes();
-  }
+//   constructor() {
+//     this.router = Router();
+//     this.userTransactionController = new UserTransactionController();
+//     this.initialiazeRoutes();
+//   }
 
-  private initialiazeRoutes() {
-    this.router.get(
-      "/:username",
-      this.userTransactionController.getUserTransaction
-    );
-  }
+//   private initialiazeRoutes() {
+//     this.router.get(
+//       "/:username",
+//       this.userTransactionController.getUserTransaction
+//     );
+//   }
 
-  getRouter(): Router {
-    return this.router;
-  }
-}
+//   getRouter(): Router {
+//     return this.router;
+//   }
+// }
