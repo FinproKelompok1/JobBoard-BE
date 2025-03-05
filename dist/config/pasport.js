652d4916f4e48f6e0b18db217dac9798a4d4461d
tsaniafarihaa <tsaniafariha31@gmail.com>
2025-03-06 04:51:38 +0700

    fix error

--- dist/config/pasport.js
+++ dist/config/pasport.js
@@ -38,7 +38,7 @@ passport_1.default.deserializeUser((id, done) => __awaiter(void 0, void 0, void 
 passport_1.default.use(new passport_google_oauth20_1.Strategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: `${process.env.BASE_URL_FE}/auth/google/callback`,
     passReqToCallback: true,
 }, (req, accessToken, refreshToken, profile, done) => __awaiter(void 0, void 0, void 0, function* () {
     var _a, _b, _c, _d;
--- src/config/pasport.ts
+++ src/config/pasport.ts
@@ -30,7 +30,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL: `${process.env.BASE_URL_FE}/auth/google/callback`,
       passReqToCallback: true,
     },
     async (req, accessToken, refreshToken, profile, done) => {
